f535393c55d9de29049f9c111698f4bc06e4f1b2
Sabinapa <sabina.pauric@gmail.com>
2023-10-08 22:18:09 +0200

    feat: added new assets image

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -12,12 +12,12 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("mjaw.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
