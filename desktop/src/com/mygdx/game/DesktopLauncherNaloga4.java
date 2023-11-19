f7747d21c8578d22b8140ffc54ea0c37b17c26e9
Sabinapa <sabina.pauric@gmail.com>
2023-11-19 22:40:20 +0100

    feat: added changes to firework particle

--- desktop/src/com/mygdx/game/DesktopLauncherNaloga4.java
+++ desktop/src/com/mygdx/game/DesktopLauncherNaloga4.java
@@ -8,10 +8,9 @@ import com.mygdx.game.Naloga4.SugarCubeGameUnits;
 public class DesktopLauncherNaloga4 {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		//config.setForegroundFPS(60);
+		config.setForegroundFPS(60);
 		config.setTitle("Are you made of sugar?");	// window title
-		//config.width = 1024;
-		//config.height = 480;
+		config.setWindowedMode(640, 480);	// window size
 
 		new Lwjgl3Application(new SugarCubeGameUnits(), config);
 	}
